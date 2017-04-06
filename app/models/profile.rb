4767d211b5715f30f6e44f1a2b86dc2e3b663f77
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-06 14:39:51 -0300

    add solution to rq14: implement get_all_profiles(min_year, max_year) class method to return all profiles within the given birth year range

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,6 +7,10 @@ class Profile < ActiveRecord::Base
   validate :first_and_last_not_null
   validate :no_male_named_sue
 
+  def self.get_all_profiles(min_year, max_year)
+    self.where('birth_year BETWEEN ? AND ?', min_year, max_year).order(:birth_year)
+  end
+
   def first_and_last_not_null
     if first_name == nil and last_name == nil
       errors.add(:first_name, "First and Last name can't be both nil")
@@ -19,4 +23,5 @@ class Profile < ActiveRecord::Base
       errors.add(:first_name, "can't have men called Sue.")
     end
   end
+
 end
