e5f5afa5641bc59407060883a6e0220013244f5e
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-06 13:47:11 -0300

    add solution to rq11: implement validation for User and Profile models including custom validators

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,22 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validates :gender, inclusion: {
+    in: %w(male female), message: "%{value} is not a valid gender."}
+
+  validate :first_and_last_not_null
+  validate :no_male_named_sue
+
+  def first_and_last_not_null
+    if first_name == nil and last_name == nil
+      errors.add(:first_name, "First and Last name can't be both nil")
+      errors.add(:last_name, "First and Last name can't be both nil")
+    end
+  end
+
+  def no_male_named_sue
+    if gender == 'male' and first_name == 'Sue'
+      errors.add(:first_name, "can't have men called Sue.")
+    end
+  end
 end
