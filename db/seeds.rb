e88c8127056fb8e44d8702637aabb82dee1da547
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-07 01:42:43 -0300

    add files from graded_assignment.zip

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
+
 User.destroy_all
-Profile.destroy_all
 TodoList.destroy_all
-TodoItem.destroy_all
 
-users = [
-  { first_name: "Carly", last_name: "Fiorina", birth_year: 1954, gender: "female" },
-  { first_name: "Donald", last_name: "Trump", birth_year: 1946, gender: "male" },
-  { first_name: "Ben", last_name: "Carson", birth_year: 1951, gender: "male" },
-  { first_name: "Hillary", last_name: "Clinton", birth_year: 1947, gender: "female" }
-]
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
 
-def generate_todo_items(todo_list_id, quantity)
-  (1..quantity).each do |i|
-    TodoItem.create! ({
-      due_date: Date.today + 1.year,
-      title: "some random title",
-      description: "some random description",
-      todo_list_id: todo_list_id
-    })
-  end
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: "Task 1", due_date: dates.sample, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: dates.sample, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: dates.sample, description: "learn Action Pack TEST", completed: true}
+  ]
 end
 
-users.each do |user|
-  Profile.create! user
-  username = user[:last_name]
-  u = User.create! ({ username: username, password_digest: "xxx" })
-  todo_list = TodoList.create! ({
-    list_name: user[:first_name] + "'s Todo List",
-    list_due_date: Date.today + 1.year,
-    user_id: u.id
-  })
-  generate_todo_items(todo_list.id, 5)
+users = User.create! [
+  { username: "jim", password: "abc123" },
+  { username: "rich", password: "123abc" }
+]
+
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
 end
