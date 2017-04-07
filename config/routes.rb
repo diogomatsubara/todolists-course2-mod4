bc7c8e6e5a677d4e2cfe12092948b78d5b05b754
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-07 11:04:28 -0300

    solution to rq10: implement sessions controller class and view allowing users to login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "todo_lists#index"
 
+  resources :sessions, only: [:new, :create, :destroy]
   resources :todo_lists do
     resources :todo_items
   end
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
 end
